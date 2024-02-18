8659c6ae56a9cef01f118f51babb6c81779ffe1c
Hum-jr <bennyangiye202@gmail.com>
2024-02-18 12:08:08 +0300

    strings

--- Strings.c
+++ Strings.c
@@ -3,11 +3,19 @@
 //
 
 #include <stdio.h>
+#include <strings.h>
 
 int main(){
     char name[40];
     printf("Enter the name: ");
     scanf("%39s",name);
+
+    char surname[]="Nyangiye";
+
+    printf("Name is: %s ",name);
+    strcpy(surname,name);
+
+    strcat(name,surname);
 
     printf("Name is: %s ",name);
     return 0;
